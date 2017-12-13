1834f083ee591e3076b684b6e54b3730d97d1b07
tamybl <tamara.bravo.l@gmail.com>
2017-12-12 23:53:40 -0300

    Muestra de imagenes por API y filtro por categoria

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
 	}
 
 	// Base de Datos API
-	var count = 0;
+	var countID = 0;
 	$.ajax({
 		type: 'GET',
 		url: 'https://developers.zomato.com/api/v2.1/search?entity_id=83&entity_type=city',
@@ -20,9 +20,27 @@ $(document).ready(function () {
 		success: function(restaurants) {
 			console.log('success', restaurants);
 			$.each(restaurants['restaurants'], function(i, restaurant) {
-				$('#list').append('<li>Local: ' + count++ + restaurant + '</li>');
+				$('#locals').append('<div class="col-xs-6 content-food"><h4 class="title">' +restaurant.restaurant.name + '</h4>' + '<img src="'+restaurant.restaurant.featured_image+'" class="img-responsive" id="img_'+ countID++ +'"><div class="cat">'+restaurant.restaurant.cuisines +'</div></div>');
+				// Para agregar todas las categorias de comida
+				/*$('#categories').append('<option>'+restaurant.restaurant.cuisines+'</option>');*/
+
 			});
+			/*$('#search').keyup(function () {
+				var keyword = $(this).val();
+				$('.content-food').hide();
+			}); */
+			/*$('.content-food').mouseenter(function () {
+				var idPic = $(this).index();*/
+			$('#categories').change(function () {
+				var keyword = $(this).val();
+				$('.content-food').hide();
+				/* :contains revisa si existe coincidencia filtrando
+				* por el keyword
+				*/
+				$(".content-food:contains('" + keyword + "')").css("display","block");
+				
+				
+			});	
 		},
-	});
-	
+	});	
 });
